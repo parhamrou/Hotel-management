909cf36f8fdab12edbda5f6d1beef2bb74d7af2d
Parham Roufarshbaf <p_roufarshbaf@yahoo.com>
2022-09-25 22:18:41 +0330

    Create class for connecting to the database.
    Also, Some classes are added to the company package with empty body.

---
+++ src/main/java/com/hotelmanagement/Database/DBConnection.java
@@ -0,0 +1,51 @@
+package com.hotelmanagement.Database;
+import java.sql.*;
+
+/**
+ * This class is for Connecting to the database and executing queries to INSERT, UPDATE and DELETE data from database;
+ */
+public class DBConnection {
+
+    private static final String DB_URL = "jdbc:mysql://localhost/hotel_management";
+    private static final String USER = "parhamrou";
+    private static final String PASS = "@Parham1381";
+    private static Connection connection;
+    private static Statement statement;
+
+    /**
+     * This method creates connection and then creates a statement to access to the database.
+     */
+    public void connect() {
+        try {
+            connection = DriverManager.getConnection(DB_URL, USER, PASS);
+            statement = connection.createStatement();
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public static boolean execute(String query) {
+        try {
+            return statement.execute(query);
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public static ResultSet executeQuery(String query) {
+        try {
+            return statement.executeQuery(query);
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public static int executeUpdate(String query) {
+        try {
+            return statement.executeUpdate(query);
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+}
---
+++ src/main/java/com/hotelmanagement/company/Food.java
@@ -0,0 +1,4 @@
+package com.hotelmanagement.company;
+
+public class Food {
+}
