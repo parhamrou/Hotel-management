19eeb3b519a85beb8a7b94e6f60dc905f5d2fb0d
Parham Roufarshbaf <p_roufarshbaf@yahoo.com>
2022-09-26 11:10:57 +0330

    Add Food class methods for connecting to the database.

--- src/main/java/com/hotelmanagement/company/Food.java
+++ src/main/java/com/hotelmanagement/company/Food.java
@@ -1,4 +1,43 @@
 package com.hotelmanagement.company;
 
+import com.hotelmanagement.Database.DBConnection;
+import com.hotelmanagement.Database.SQLQueries;
+
 public class Food {
+
+    private int id;
+    private String name;
+    private int price;
+
+    public Food(int id, String name, int price) {
+        this.id = id;
+        this.name = name;
+        this.price = price;
+    }
+
+    public void remove() {
+        DBConnection.execute(SQLQueries.delete("food", "food_id = " + id));
+    }
+
+    public void setName(String name) {
+        DBConnection.execute(SQLQueries.update("food", "food_name = " + name, "food_id = " + id));
+        this.name = name;
+    }
+
+    public void setPrice(int price) {
+        DBConnection.execute(SQLQueries.update("food", "price = " + price, "food_id = " + id));
+        this.price = price;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public int getPrice() {
+        return price;
+    }
+
+    public String getName() {
+        return name;
+    }
 }
